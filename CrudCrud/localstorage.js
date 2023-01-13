a8c3a2bf2416412fa5726b223abedf0ec2ec336e
Ganeshpk96 <ganeshpk96@gmail.com>
2023-01-13 14:36:24 +0530

    Saving in CRud

---
+++ CrudCrud/localstorage.js
@@ -0,0 +1,10 @@
+
+// var object = {
+//     name: 'John',
+//     age: 30
+// }
+// var myObj = JSON.stringify(object)
+// localStorage.setItem('power', myObj);
+
+// var getObj = JSON.parse(localStorage.getItem('power'));
+// console.log(getObj);
